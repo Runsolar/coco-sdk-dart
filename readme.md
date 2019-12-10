78663cf951ba579ffab2e1487cfe28203425c8fb
WDA <engineer13@outlook.com>
2019-12-10 12:39:21 +0300

    wda

--- readme.md
+++ readme.md
@@ -1,5 +1,2 @@
 CoCo (Conversational Components) SDK
 https://www.conversationalcomponents.com
-
-installation:
-npm install @aleximp/coco-sdk-nodejs
--- test.js
+++
@@ -1,4 +0,0 @@
-const sdk = require("./lib/index");
-
-const conv = new sdk.ComponentSession("namer_vp3", "test");
-conv.call("Hola").then(r => console.dir(r));
